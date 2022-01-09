2490bb5010b386d89e41f17424770979d3bc0b2c
oguz senna <ogzsnn55@gmail.com>
2022-01-10 00:16:22 +0300

    final

--- HW1.sql
+++ HW1.sql
@@ -1,3 +1,5 @@
 select title,description  from film;
 select * from film where length>60 and length<75;
 select * from film where rental_rate=0.99 and replacement_cost=12.99 or replacement_cost=28.99;
+select last_name from customer where first_name='Mary';
+select* from film where not length>50 and rental_rate !=2.99 or rental_rate!=4.99;
